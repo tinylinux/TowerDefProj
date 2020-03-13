a485956d90d04b9f7054db963ae62f4cf973a77b
TinyLinux <ridalal23@hotmail.fr>
2020-03-13 17:40:21 +0100

    Implémentation de MapGenerator

--- src/main/scala/jeu/test.scala
+++ src/main/scala/jeu/test.scala
@@ -8,13 +8,12 @@
 package jeu
 
 import affichage._
-
-
+import scala.io.Source
 
 class CarteTest extends Carte {
 
   // définition de la carte
-  /*
+  /**
    *   0 1 2 3 4 5 6 7 8 9
    * 0 X X X X X X X X X X
    * 1 X S O O O O O O P X
@@ -27,24 +26,56 @@ class CarteTest extends Carte {
 
    */
 
-  maxX = 9
-  maxY = 2
+   def generateElements(i:Char):Case = {
+     if (i == 'X')
+     {
+       new Mur()
+     }
+     else
+     {
+       new Sol()
+     }
+   }
 
+  val filename = "src/main/resources/maps/test.txt"
+  val filelines = Source.fromFile(filename).getLines.toArray
 
-  cases = Array(
+  // On suppose que le fichier est un fichier correct
+  // (éventuellement on pourrait implémenter une fonction qui vérifie que le fichier est correct)
+
+  maxX = filelines(0).length - 1
+  maxY = filelines.length - 1
+
+  cases = Array.ofDim[Case](maxY+1, maxX+1)
+
+  // mise en place de la tourPrincipale
+  override val tourPrincipale = new TourPrincipale(this, 10)
+
+  for (l <- filelines.indices) {
+    for (c <- filelines(l).indices) {
+      // Création de la case
+      cases(l)(c) = generateElements(filelines(l)(c));
+
+      // Création de la Tour Principale
+      if (filelines(l)(c) == 'P')
+      {
+        spawnTour(tourPrincipale, (c,l))
+      }
+    }
+  }
+
+  /** cases = Array(
     Array(Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur()),
     Array(Mur(), Sol(), Sol(), Sol(), Sol(), Sol(), Sol(), Sol(), Sol(), Mur()),
     Array(Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur(), Mur())
-  )
+  ) */
 
 
   // apparition des ennemis
   spawnEnnemi(new Ennemi(this, 5))
   spawnEnnemi(new Ennemi(this, 7))
+  spawnEnnemi(new Ennemi(this, 10))
 
-  // mise en place de la tourPrincipale
-  override val tourPrincipale = new TourPrincipale(this, 10)
-  spawnTour(tourPrincipale, (8,1))
 
   // mise en place d'une tour
   spawnTour(new TourAttaque(this, 1, 10), (5,2))
